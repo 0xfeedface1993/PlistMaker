f791d2a88fa0b9e03d25e4a25d792e76ec2077d7
许庭淞 <318715498@qq.com>
2018-05-08 11:28:36 +0800

    touch bar support

--- H5ProjectMake/PlistMaker/CustomWindowController.swift
+++ H5ProjectMake/PlistMaker/CustomWindowController.swift
@@ -15,32 +15,5 @@ class CustomWindowController: NSWindowController {
     
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
     }
-
-    @IBAction func generation(_ sender: Any) {
-        findMaker().generation(self)
-    }
     
-    @IBAction func bundleIdentifier(_ sender: Any) {
-        findMaker().bundleIdentifier.becomeFirstResponder()
-    }
-    
-    @IBAction func imageNmae(_ sender: Any) {
-        findMaker().imageName.becomeFirstResponder()
-    }
-    
-    @IBAction func appNmae(_ sender: Any) {
-        findMaker().appName.becomeFirstResponder()
-    }
-    
-    @IBAction func ipa(_ sender: Any) {
-        findMaker().ipaDirPath.becomeFirstResponder()
-    }
-    
-    @IBAction func imageDir(_ sender: Any) {
-        findMaker().imageDirPath.becomeFirstResponder()
-    }
-    
-    func findMaker() -> PlistViewController {
-        return contentViewController as! PlistViewController
-    }
 }
